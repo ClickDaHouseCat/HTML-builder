0d0bbc338b4aec5bb9cf305cf9e5bcacf5786f56
Evgenii Dementev <63846200+ClickDaHouseCat@users.noreply.github.com>
2024-01-20 23:40:52 +0300

    fix: fix end script

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const readline = require('readline');
 
-const filePath = './02-write-file/newText.txt';
+const mainFilePath = './02-write-file/Text.txt';
 
 function fileHandler() {
   // Приглашение для ввода текста в консоли
@@ -10,17 +10,24 @@ function fileHandler() {
     output: process.stdout
   });
 
-  rl.question('Введите текст для записи в файл: ', (userInput) => {
-    // Запись в файл
-    fs.writeFile(filePath, userInput, 'utf8', (err) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      console.log(`Текст был успешно записан в файл: ${filePath}`);
-      rl.close();
+  function handleUserInput() {
+    rl.question('Введите текст для записи в файл: ', (userInput) => {
+      // Добавление текста к существующему файлу
+      fs.appendFile(mainFilePath, userInput + '\n', 'utf8', (err) => {
+        if (err) {
+          console.error(err);
+          rl.close();
+          return;
+        }
+        console.log(`Текст был успешно добавлен к файлу: ${mainFilePath}`);
+        // Повторный вызов для ожидания нового ввода
+        handleUserInput();
+      });
     });
-  });
+  }
+
+  // Начало процесса ввода текста
+  handleUserInput();
 }
 
 fileHandler();
