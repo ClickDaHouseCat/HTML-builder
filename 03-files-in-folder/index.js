05441ac56214acfc25623f41dcd9e2148a50e573
Evgenii Dementev <63846200+ClickDaHouseCat@users.noreply.github.com>
2024-01-24 22:33:29 +0300

    fix: del info about folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,33 +1,34 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 const folderPath = './03-files-in-folder/secret-folder';
 
-function listFilesInFolder() {
-  // Чтение содержимого папки
-  fs.readdir(folderPath, (err, files) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
+async function listFilesInFolder() {
+  try {
+    // Чтение содержимого папки
+    const files = await fs.readdir(folderPath);
 
     // Обработка каждого файла в папке
-    files.forEach((file) => {
+    const fileStatsPromises = files.map(async (file) => {
       const filePath = path.join(folderPath, file);
 
       // Получение информации о файле
-      fs.stat(filePath, (err, stats) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
+      const stats = await fs.stat(filePath);
 
+      // Проверка, является ли текущий элемент файлом
+      if (stats.isFile()) {
         // Вывод информации в нужном формате
         const fileNameWithoutExtension = path.basename(file, path.extname(file));
         console.log(`${fileNameWithoutExtension}-${path.extname(file).slice(1)}-${stats.size} bytes`);
-      });
+      }
     });
-  });
+
+    // Дождитесь завершения всех операций fs.stat
+    await Promise.all(fileStatsPromises);
+
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 listFilesInFolder();
